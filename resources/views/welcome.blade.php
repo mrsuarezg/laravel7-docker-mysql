d7a0b63eddc133a3bb7c06f19988ca1e77d621db
SuarezDev <cesar.bejucos.2014@gmail.com>
2022-11-28 13:50:32 -0600

    added vue 2 and laravel /ui

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="{{asset('css/app.css')}}" rel="stylesheet">
 
         <!-- Styles -->
         <style>
@@ -62,6 +63,7 @@
                 margin-bottom: 30px;
             }
         </style>
+        <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
@@ -95,6 +97,9 @@
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
+        </div>
+        <div id="app">
+            <example-component />
         </div>
     </body>
 </html>
